6f0cba825d318744a7767810051da1449a4d1105
Alam <13517096@std.stei.itb.ac.id>
2019-11-22 19:39:18 +0700

    Added interface for transfer SOAP

--- src/utils/jaxws.js
+++ src/utils/jaxws.js
@@ -29,3 +29,34 @@ export async function checkLoginCredentials(accNum) {
     console.log(e.stack);
   }
 }
+
+export async function doTransfer(fromAcc, toAcc, nom){
+  const config={
+    headers:{
+      "Content-Type": "text/xml"
+    }
+  };
+  const xmlContent = `
+    <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:k03="http://K03G04Tubes2.org/">
+      <soapenv:Header/>
+      <soapenv:Body>
+        <k03:createTransaksiTransfer>
+          <account_num_pengirim>${fromAcc}</account_num_pengirim>
+          <account_numorva_penerima>${toAcc}</account_numorva_penerima>
+          <jlh_uang>${nom}</jlh_uang>
+        </k03:createTransaksiTransfer>
+      </soapenv:Body>
+    </soapenv:Envelope>
+  `;
+
+  try{
+    const res = await Axios.post(wsdlURI,xmlContent,config);
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(res.data,"text/xml");
+    const result = xmlDoc.getElementsByTagName("return")[0].innerHTML;
+    return result;
+  } catch {e}{
+    console.log(e.stack);
+    return "Error , probably 500 or 400 idk. Check in console";
+  }
+}
